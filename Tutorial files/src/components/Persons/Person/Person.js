14998d8c4b5629589b578823b5eb3f9444b4f3a4
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-04-25 10:11:58 +0530

    Using Auxillary (hoc) instead of returning HTML elements as an Array
    IN PERSON . JS
              instead of returning HTML elements as an array wrapped within [ ] and giving individual keys for each HTML element , we can create HIGHER ORDER COMPONENTS ( Auxillary ) which returns the children of the props given to it.
              just create Auxillary.js and in person.js return the HTML elements within <Auxillary></Auxillary> after importing it.

--- Tutorial files/src/components/Persons/Person/Person.js
+++ Tutorial files/src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
 import React , { Component } from 'react';
 import classes from './Person.css';
+import Auxillary from '../../../hoc/Auxillary'
 
 class Person extends Component {
   render() {
     console.log('Person.js] rendering...')
-      return [
+      return( 
         // <div className={classes.Person}>
-          <p key="i1" onClick={this.props.click}>
-            I'm {this.props.name} and I am {this.props.age} years old!
-          </p>,
-          <p key="i2" >{this.props.children}</p>,
-          <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+        <Auxillary>
+            <p key="i1" onClick={this.props.click}>
+              I'm {this.props.name} and I am {this.props.age} years old!
+            </p>
+            <p key="i2" >{this.props.children}</p>
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+        </Auxillary>
         // </div>
-        ];
+        );
   }
 };
 
