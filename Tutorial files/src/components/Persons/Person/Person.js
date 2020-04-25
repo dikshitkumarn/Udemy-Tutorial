568601d1b0c0d2c36c0709271be7a280330f5e7f
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-04-25 10:52:16 +0530

    WithClass.js
    IN APP. JS AND APP . JS
               wrap the div to which you use class with <WithClass> </WithClass>
    which you import from WithClass.js
    
    IN WITHCLASS . JS
             make a div and assign it a class from the props and return the children inside the div

--- Tutorial files/src/components/Persons/Person/Person.js
+++ Tutorial files/src/components/Persons/Person/Person.js
@@ -1,19 +1,20 @@
 import React , { Component , Fragment } from 'react';
 import classes from './Person.css';
 import Auxillary from '../../../hoc/Auxillary'
+import WithClass from '../../../hoc/WithClass'
 
 class Person extends Component {
   render() {
     console.log('Person.js] rendering...')
       return( 
         // <div className={classes.Person}>
-        <Fragment>
+        <WithClass classes={classes.Person} >
             <p key="i1" onClick={this.props.click}>
               I'm {this.props.name} and I am {this.props.age} years old!
             </p>
             <p key="i2" >{this.props.children}</p>
             <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
-        </Fragment>
+        </WithClass>
         // </div>
         );
   }
