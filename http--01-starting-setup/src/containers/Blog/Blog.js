4765e09498587265f6429933d8a0de7309fa5219
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-10 09:56:57 +0530

    Actually Added Burger builder part1 and Backend startup now (study description)
    in backend i finished get, post, delete, error handling

---
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedId:null,
+        error:false
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/postsssssssssss')
+        .then(res => {
+            const posts = res.data.slice(0,4)
+            const updatedposts = posts.map(
+                post => {
+                    return {
+                        ...post,
+                        author:'Dikshit'
+                    }
+                }
+            )
+            this.setState({posts: updatedposts})
+            // console.log(res)
+        })
+        .catch(error => {
+            this.setState({error:true})
+            // console.log(error)
+        })
+    }
+
+    postSelected= id => {
+        this.setState({selectedId:id})
+    }
+
+    render () {
+        let posts = <p>Somethig Went Wrong</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(
+                post => {
+                    return <Post key={post.id} title={post.title} Author={post.author} clicked={() => this.postSelected(post.id)} />
+                }
+            )
+        }
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedId} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
