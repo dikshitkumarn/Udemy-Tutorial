5601fff0600bf70b44a393c3f4d15b36cb262ef8
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-10 12:18:24 +0530

    Using Defaults in aixios
    Default headers , doctype,etc...

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
         if(this.props.id){
             if( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ){
             // if( !this.state.loadedPost || (this.state.loadedPost.id !== this.props.id) ){
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id )
+                axios.get('/posts/' + this.props.id )
                 .then(res => {
                     this.setState({loadedPost: res.data})
                     // console.log(res)
@@ -23,7 +23,7 @@ class FullPost extends Component {
     }
 
     deletePost = () => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/'+ this.props.id)
+        axios.delete('/posts/'+ this.props.id)
         .then(res => console.log(res))
     }
 
