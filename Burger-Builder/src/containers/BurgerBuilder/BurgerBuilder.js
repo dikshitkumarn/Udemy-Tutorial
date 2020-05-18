979e9562f9d6bd61a2d0b16f140d4624899b992a
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-18 13:55:55 +0530

    Fixed Order button to work properly without using redux.

--- Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,6 @@ class BurgerBuilder extends Component {
     //     this.state = {...}
     // }
     state = {
-        purchasable: false,
         purchasing: false,
         loading:false,
         error: null
@@ -41,7 +40,7 @@ class BurgerBuilder extends Component {
             .reduce( ( sum, el ) => {
                 return sum + el;
             }, 0 );
-        this.setState( { purchasable: sum > 0 } );
+        return sum > 0  ;
     }
 
     // addIngredientHandler = ( type ) => {
@@ -116,7 +115,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.props.onAddIngredient}
                     ingredientRemoved={this.props.onDeleteIngredient}
                     disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
+                    purchasable={this.updatePurchaseState(this.props.ingredients)}
                     ordered={this.purchaseHandler}
                     price={this.props.totalPrice} />
             </Aux>
