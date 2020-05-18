ed8ef67ba65e5a0b7929b38427fa1f7cdad52edf
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-18 12:21:50 +0530

    In Burger Builder application decided which state elements should use Redux.
    Created Reducer and actions file for BurgerBuilder component.

--- Burger-Builder/src/index.js
+++ Burger-Builder/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom'
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
 
 const app = (
     <BrowserRouter><App /></BrowserRouter>
