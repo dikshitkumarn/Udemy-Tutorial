6ac6d1138ff0d7606933e8fa49fc9ec79060fd47
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-19 19:52:10 +0530

    Made Switch case leaner

--- Redux/src/store/reducers/result.js
+++ Redux/src/store/reducers/result.js
@@ -5,22 +5,25 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action)  => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+    return updateObject(state, {results: updatedArray})
+}
+
 const reducer = ( state = initialState, action ) => {
     // SWITCH
     // Do simple logics Here like res * 2
     switch ( action.type ){
-        case( actionTypes.STORE_RESULT ):
-            return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.counter *2 })})
+        case( actionTypes.STORE_RESULT ): return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.counter *2 })})
+            
             // return {
             //     ...state,
             //     results: state.results.concat({ id: new Date(), value: action.counter *2 })
             // }
-        case( actionTypes.DELETE_RESULT ):
+        case( actionTypes.DELETE_RESULT ): return deleteResult(state, action)
             // let id = 2
             // let newArray = [...state.results]
             // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-            return updateObject(state, {results: updatedArray})
             // return {
             //     ...state,
             //     results: updatedArray
