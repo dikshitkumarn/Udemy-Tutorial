9b9998fc60bbc5597a78652378794de102b32fd7
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-19 13:29:43 +0530

    Used a utility function updateObject.
    To make code leaner we made a new function which takes oldState and newState as arguments and returns a updated object.

--- Redux/src/store/reducers/result.js
+++ Redux/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from "../utility/utility";
 
 const initialState = {
     results: []
@@ -9,19 +10,21 @@ const reducer = ( state = initialState, action ) => {
     // Do simple logics Here like res * 2
     switch ( action.type ){
         case( actionTypes.STORE_RESULT ):
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: action.counter *2 })
-            }
+            return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.counter *2 })})
+            // return {
+            //     ...state,
+            //     results: state.results.concat({ id: new Date(), value: action.counter *2 })
+            // }
         case( actionTypes.DELETE_RESULT ):
             // let id = 2
             // let newArray = [...state.results]
             // newArray.splice(id, 1)
             const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, {results: updatedArray})
+            // return {
+            //     ...state,
+            //     results: updatedArray
+            // }
     }
 
     return state
