0cf6fe6f4e65c45779e9437618b5949360b6dcf7
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-21 14:24:59 +0530

    SImplified code

--- Burger-Builder/src/store/reducers/burgerBuilder.js
+++ Burger-Builder/src/store/reducers/burgerBuilder.js
@@ -14,33 +14,47 @@ const initialState = {
     error: false
 }
 
+const addIngredient = (state, action) => {
+        let updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+        let updatedIngredients = updateObject(state.ingredients, updatedIngredient)
+        let updatedState = {
+            ingredients: updatedIngredients,
+            totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        }
+        return updateObject(state, updatedState)
+}
+
+const deleteIngredient = (state, action) => {
+        let updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+        let updatedIngs = updateObject(state.ingredients, updatedIng)
+        let updatedSt = {
+            ingredients: updatedIngs,
+            totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        }
+        return updateObject(state, updatedSt)
+}
+
+const initIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: action.ingredients,
+        totalPrice: 4,
+        error:false
+    })
+}
+
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject(state, {error: action.error})
+}
+
 const burgerBuilderReducer = (state = initialState, action) => {
     switch (action.type){
-        case (actionTypes.ADD_INGREDIENT):
-            let updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
-            let updatedIngredients = updateObject(state.ingredients, updatedIngredient)
-            let updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updateObject(state, updatedState)
-        case (actionTypes.REMOVE_INGREDIENT):
-            let updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
-            let updatedIngs = updateObject(state.ingredients, updatedIng)
-            let updatedSt = {
-                ingredients: updatedIngs,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updateObject(state, updatedSt)
-
-        case (actionTypes.INIT_INGREDIENTS):
-            return updateObject(state, {
-                ingredients: action.ingredients,
-                totalPrice: 4,
-                error:false})
+        case (actionTypes.ADD_INGREDIENT): return addIngredient(state, action)
+            
+        case (actionTypes.REMOVE_INGREDIENT): return deleteIngredient(state,action)
+            
+        case (actionTypes.INIT_INGREDIENTS): return initIngredients(state, action)            
                 
-        case(actionTypes.FETCH_INGREDIENTS_FAILED):
-                return updateObject(state, {error: action.error})
+        case(actionTypes.FETCH_INGREDIENTS_FAILED): return fetchIngredientsFailed(state, action)
                 
         default:
             return state
--- Burger-Builder/src/store/reducers/order.js
+++ Burger-Builder/src/store/reducers/order.js
@@ -7,30 +7,53 @@ const initialState = {
     purchased: false
 }
 
+const submitStart = (state, action) => {
+    return updateObject(state, {loading: true})
+}
+
+const submitSuccess = (state, action) => {
+        let newOrder = {...action.orderData} 
+        newOrder = updateObject(newOrder, {id: action.id})
+        return updateObject(state, {orders: state.orders.concat(newOrder), loading: false, purchased: true})
+}
+
+const submitFailure = (state, action) => {
+    return updateObject(state, {loading: false, purchased: false}) 
+}
+
+const submitStarted = (state, action) => {
+    return updateObject(state, {purchased: false})
+}
+
+const fetchOrderInit = (state, action) => {
+    return updateObject(state, {loading:true})
+}
+
+const fetchOrderSuccess = (state, action) => {
+    return updateObject(state, {orders: action.orders, loading: false})
+}
+
+const fetchOrderFail = (state, action) => {
+    return updateObject(state, {loading: false})
+}
+
 const orderSubmit = (state = initialState, action) => {
+
     switch (action.type){
-        case(actionTypes.SUBMIT_START):
-            return updateObject(state, {loading: true})
+        
+        case(actionTypes.SUBMIT_START): return submitStart(state, action)
 
-        case(actionTypes.SUBMIT_SUCCESS):
-            let newOrder = {...action.orderData} 
-            newOrder = updateObject(newOrder, {id: action.id})
-            return updateObject(state, {orders: state.orders.concat(newOrder), loading: false, purchased: true})
+        case(actionTypes.SUBMIT_SUCCESS): return submitSuccess(state, action)
 
-        case(actionTypes.SUBMIT_FAILURE):
-            return updateObject(state, {loading: false, purchased: false}) 
+        case(actionTypes.SUBMIT_FAILURE): return submitFailure(state, action)
 
-        case(actionTypes.SUBMIT_STARTED):
-            return updateObject(state, {purchased: false})
+        case(actionTypes.SUBMIT_STARTED): return submitStarted(state, action)
 
-        case(actionTypes.FETCH_ORDER_INIT):
-            return updateObject(state, {loading:true})
+        case(actionTypes.FETCH_ORDER_INIT): return fetchOrderInit(state, action)
 
-        case(actionTypes.FETCH_ORDER_SUCCESS):
-            return updateObject(state, {orders: action.orders, loading: false})
+        case(actionTypes.FETCH_ORDER_SUCCESS): return fetchOrderSuccess(state, action)
 
-        case(actionTypes.FETCH_ORDER_FAIL):
-            return updateObject(state, {loading: false})
+        case(actionTypes.FETCH_ORDER_FAIL): return fetchOrderFail(state, action)
 
         default:
             return state
