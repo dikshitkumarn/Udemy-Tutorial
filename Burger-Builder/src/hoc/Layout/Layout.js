dc18183f7a4d5c59d8c344fd208a4a3a3c14d5f7
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-23 09:24:05 +0530

    Added Logout route
    It will be displayed when the user is Authenticated.

--- Burger-Builder/src/hoc/Layout/Layout.js
+++ Burger-Builder/src/hoc/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
 import Aux from '../_Aux/_Aux';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import * as actions from '../../store/actions/index'
 
 class Layout extends Component {
     state = {
@@ -23,10 +25,12 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} isAuth={ this.props.isAuth } onClick= {this.props.onAuthenticated} />
                 <SideDrawer
                     open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler} />
+                    closed={this.sideDrawerClosedHandler} 
+                    isAuth={ this.props.isAuth }
+                    onClick= {this.props.onAuthenticated} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -35,4 +39,16 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuth: state.auth.token !== null
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuthenticated: () => dispatch(actions.logout())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
