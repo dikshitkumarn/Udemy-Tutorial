8803a991d88f23303f73989599b088b5bb111930
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-24 12:26:39 +0530

    Added lazy loafing for auth,orders,checkout pages

--- Burger-Builder/src/App.js
+++ Burger-Builder/src/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+// import Checkout from './containers/Checkout/Checkout';
+// import Orders from './containers/Orders/Orders';
+// import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+const asyncCheckout = asyncComponent(() => import('./containers/Checkout/Checkout'))
+const asyncAuth = asyncComponent(() => import('./containers/Auth/Auth'))
+const asyncOrders = asyncComponent(() => import('./containers/Orders/Orders'))
+
+// const LazyCheckout = React.lazy(() => {
+//   return import('./containers/Checkout/Checkout')
+// })
+
+// const LazyAuth = React.lazy(() => {
+//   return import('./containers/Auth/Auth')
+// })
+
+// const LazyOrders = React.lazy(() => {
+//   return import('./containers/Auth/Auth')
+// })
 
 class App extends Component {
   componentDidMount () {
@@ -18,7 +35,8 @@ class App extends Component {
   render () {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        {/* <Route path="/auth" render={() => <Suspense fallback={<div>Loading...</div>} ><LazyAuth /></Suspense>} /> */}
+        <Route path="/auth" component={asyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -27,10 +45,13 @@ class App extends Component {
     if ( this.props.isAuthenticated ) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          {/* <Route path="/checkout" render={() => <Suspense fallback={<div>Loading...</div>} ><LazyCheckout /></Suspense>} /> */}
+          <Route path="/orders" component={asyncOrders} />
+          {/* <Route path="/orders" render={() => <Suspense fallback={<div>Loading...</div>} ><LazyOrders /></Suspense>} /> */}
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
+          {/* <Route path="/auth" render={() => <Suspense fallback={<div>Loading...</div>} ><LazyAuth /></Suspense>} /> */}
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
