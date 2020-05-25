77a275e4e77cc150c3678b26c37556b59941e2b3
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-25 17:52:39 +0530

    Created a basic next js file
    Server Side rendering
    
    npm i next react react-dom
    
    >pages
          index.js     //this will run in domainname.com/
        >auth    //this will run in domainname.com/auth
           index.js
    
    now run   ->  npm run dev
    
    server will run in localhost:3000

---
+++ Next JS/pages/auth/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const authIndex = () => (
+    <h1>This is Auth Index Page</h1>
+)
+
+export default authIndex
---
+++ Next JS/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const homePage = () => (
+    <h1>This is Home Page</h1>
+)
+
+export default homePage
