2ec4407a7b26dff9ba7ffd930d4bd9ec6905700a
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-26 16:55:48 +0530

    Using our own classNames in different time of animation happening
    In CSSTransition Component the prop classNames can be an object,
    
    eg:
    
    classNames={{
                enter:'',
                enterActive: 'ModalOpen',
                exit: "",
                exitActive: "ModalClose",
                appear: '',
                appearActive:''
            }}

--- CSS animations/src/components/Modal/Modal.js
+++ CSS animations/src/components/Modal/Modal.js
@@ -14,7 +14,16 @@ const animationTime = {
 const modal = (props) => {
 
     return(
-        <CSSTransition in={props.show} timeout={animationTime} mountOnEnter unmountOnExit classNames="fade-slide" >
+        // <CSSTransition in={props.show} timeout={animationTime} mountOnEnter unmountOnExit classNames="fade-slide" >
+        <CSSTransition in={props.show} timeout={animationTime} mountOnEnter unmountOnExit 
+        classNames={{
+            enter:'',
+            enterActive: 'ModalOpen',
+            exit: "",
+            exitActive: "ModalClose",
+            appear: '',
+            appearActive:''
+        }} >
             <div className={"Modal"}>
                 <h1>A Modal</h1>
                 <button className="Button" onClick={props.closed}>Dismiss</button>
