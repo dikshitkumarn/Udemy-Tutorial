67c857e2f3e2208ddd0c03d2f8d9e2f8a08767c7
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-27 17:15:53 +0530

    Optimized request amount (also used useRef( )
    now with setTimeout() method we optimized the no.of requests we send.

--- React Hooks/src/components/Ingredients/Search.js
+++ React Hooks/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,24 +6,31 @@ import './Search.css';
 const Search = React.memo(props => {
 
   const [searchValue, setSearchValue] = useState("")
+  const input = useRef()
   const { onUpdate } = props
 
   useEffect(() => {
-    const query = searchValue.length === 0 ? '' : `?orderBy="title"&equalTo="${searchValue}"`
-    fetch('https://react-hooks-a4367.firebaseio.com/ingredients.json' + query )
-    .then(res => {
-      return res.json()
-    })
-    .then(resData => {
-      let ingredientsArray = []
-      for (const key in resData){
-        ingredientsArray.push({
-          id:key,
-          ...resData[key]
+
+    setTimeout(() => {
+      if(searchValue === input.current.value ){
+        const query = searchValue.length === 0 ? '' : `?orderBy="title"&equalTo="${searchValue}"`
+        fetch('https://react-hooks-a4367.firebaseio.com/ingredients.json' + query )
+        .then(res => {
+          return res.json()
         })
-      }
-      onUpdate([...ingredientsArray])
-    })
+        .then(resData => {
+          let ingredientsArray = []
+          for (const key in resData){
+            ingredientsArray.push({
+              id:key,
+              ...resData[key]
+            })
+          }
+          onUpdate([...ingredientsArray])
+        })
+          }
+        }, 500);
+    
   }, [searchValue, onUpdate])
 
   const updateSearchValue = (event) => {
@@ -34,7 +41,7 @@ const Search = React.memo(props => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={searchValue} onChange={event => updateSearchValue(event)} />
+          <input ref={input} type="text" value={searchValue} onChange={event => updateSearchValue(event)} />
         </div>
       </Card>
     </section>
