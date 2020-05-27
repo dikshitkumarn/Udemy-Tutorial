0aabf50e9c5fd91c2f691d02cd20895a2591c42e
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-27 09:42:39 +0530

    Paused Redux-Saga and jumped into React Hooks

--- Burger-Builder/src/store/actions/auth.js
+++ Burger-Builder/src/store/actions/auth.js
@@ -35,6 +35,12 @@ export const logout = () => {
     }
 };
 
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
 export const checkAuthTimeout = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
