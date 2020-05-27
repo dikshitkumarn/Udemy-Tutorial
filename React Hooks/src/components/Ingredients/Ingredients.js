a9352917546d1f0e49cc71ad084675aa255872e3
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-05-27 18:37:24 +0530

    Injected loading and error handling

--- React Hooks/src/components/Ingredients/Ingredients.js
+++ React Hooks/src/components/Ingredients/Ingredients.js
@@ -3,29 +3,42 @@ import React, { useState, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList'
+import Modal from '../UI/ErrorModal'
 
 const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([])
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const addIngredient = ingredient => {
+    setLoading(true)
     fetch('https://react-hooks-a4367.firebaseio.com/ingredients.json',{
       method: 'POST',
       headers: {'Context-Type': 'application/json'},
       body: JSON.stringify(ingredient)
     }).then(res => {
+      setLoading(false)
         return res.json()
     }).then(resData => {
         setIngredients(prevState => [...prevState, {id: resData.name, ...ingredient}])
+    }).catch(error => {
+      setLoading(false)
+      setError('Something went wrong')
     })
   }
 
   const removeIngredient = id => {
+    setLoading(true)
     fetch(`https://react-hooks-a4367.firebaseio.com/ingredients/${id}.json`,{
       method: 'DELETE'
     })
     .then(res => {
+      setLoading(false)
       setIngredients(prevIngredients => prevIngredients.filter(curr => curr.id !== id))
+    }).catch(error => {
+      setLoading(false)
+      setError('Something went wrong')
     })
   }
 
@@ -33,9 +46,14 @@ const Ingredients = () => {
     setIngredients(filteredIngredientsArray)
   },[])
 
+  const modalClose = () => {
+    setError(null)
+  }
+
   return (
     <div className="App">
-      <IngredientForm  onAddIngredient = {addIngredient} />
+      {error && <Modal onClose={modalClose} > {error} </Modal>}
+      <IngredientForm isLoading={isLoading} onAddIngredient = {addIngredient} />
 
       <section>
         <Search onUpdate={filteredIngredients} />
